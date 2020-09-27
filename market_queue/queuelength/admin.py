275ec91768c31c4045df126cd3a464cdf546de87
Amitkumar-sde <71953221+Amitkumar-sde@users.noreply.github.com>
2020-09-27 08:21:51 +0530

    Update admin.py

--- market_queue/queuelength/admin.py
+++ market_queue/queuelength/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import store_queue
-from .models import store_queue_history
+from .models import queue_info
 
-admin.site.register(store_queue)
-admin.site.register(store_queue_history)
+
+admin.site.register(queuelength)
 
